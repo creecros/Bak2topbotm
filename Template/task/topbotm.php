fa0230594bcc1644f5eda803e2f1518cf4e5c98d
Craig Crosby <creecros@gmail.com>
2018-09-27 13:37:17 -0400

    Update topbotm.php

--- Template/task/topbotm.php
+++ Template/task/topbotm.php
@@ -1,2 +1,2 @@
-<button onclick="topFunction()" id="scrollBtn" title="Go to top">Top</button>
-<button onclick="topFunction()" id="scrollBtn2" title="Go to top">Top</button>
+<button id="scrollBtn" title="Go to top">Top</button>
+<button id="scrollBtn2" title="Go to Bottom">Top</button>
