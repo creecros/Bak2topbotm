b15cd9898d3e334a1791c4ef64a296f14114271e
Craig Crosby <creecros@gmail.com>
2018-09-27 12:30:07 -0400

    Update topbotm.js

--- topbotm.js
+++ topbotm.js
@@ -1,16 +1,18 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scrollBtn").style.display = "block";
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
     } else {
-        document.getElementById("scrollBtn").style.display = "none";
+        $('#back2Top').fadeOut();
     }
-}
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+});
+ /*Scroll to top when arrow up clicked END*/
